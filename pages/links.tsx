02f7539ce787005bf3bf99e76924a379226d80c3
Alex Ho <87953031+gurulantern@users.noreply.github.com>
2024-03-31 14:13:19 -0400

    Components

---
+++ pages/links.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from "../styles/Links.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+const Links: React.FC = () => {
+  return (
+    <div className={styles.container}>
+        <a href='https://www.linkedin.com/in/alex-ho-a9471833/'>
+            <FontAwesomeIcon className={styles.icon} icon={faLinkedin} />
+        </a>     
+        <a href='https://github.com/gurulantern'>
+            <FontAwesomeIcon className={styles.icon} icon={faGithub} />
+        </a>
+    </div>
+  )
+}
+
+export default Links
---
+++ pages/portfolio.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PostList from './postlist';
+
+const posts = [
+    { slug: 'codereview', title: 'Code Review' },
+    { slug: 'artifact1', title: 'Software Design and Engineering' },
+    { slug: 'artifact2', title: 'Algorithms & Data Structures' },
+    { slug: 'artifact3', title: 'Databases' },
+];
+
+const Portfolio: React.FC = () => {
+  return (
+    <div>
+      <h3>SNHU Portfolio</h3>
+      <PostList posts={posts} />
+    </div>
+  );
+};
+
+export default Portfolio;

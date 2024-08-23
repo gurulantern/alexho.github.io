dac6d959b992cd81d8292380758e9b4b8fb7d683
Alex Ho <87953031+gurulantern@users.noreply.github.com>
2024-08-23 10:08:27 -0400

    web update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -25,21 +25,6 @@ export default {
     </footer>
   ),
   navs: [
-    {
-      url: 'https://gurulantern.github.io/',
-      name: 'Home'
-    },
-    {
-      url: 'https://gurulantern.github.io/about',
-      name: 'About'
-    },
-    {
-      url: 'https://gurulantern.github.io/portfolio',
-      name: 'School Portfolio'
-    },
-    {
-      url: 'https://gurulantern.github.io/summerhell-survivor',
-      name: 'Game 1'
-    },
+
   ]
 };
